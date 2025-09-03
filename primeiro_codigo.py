d1ec2a5ed6d4a9cce1fe082b62a074b660f03e94
kezia-083 <kezialays.ketlyn@gmail.com>
2025-09-03 18:14:54 -0300

    Add files via upload

---
+++ primeiro_codigo.py
@@ -0,0 +1,20 @@
+print ("kezia")
+nome = "maria"
+idade = 20
+print (f"olá meu nome é {nome}, tenho {idade} ")
+cidade = "campina"
+estado = "paraíba"
+print (f"moro em {cidade},na {estado}")
+usuario = "promação"
+print (f" você escolheu o curso {usuario}")
+filme = "barbie"
+ano = 1990
+print (f"meu filme favorito é {filme}, que foi lançado em {ano}")
+numero1 = 5 + 8
+print (" a soma é: ", numero1)
+numero2 = 10 - 5
+print("a subtração é", numero2)
+numero3 = 3*7
+print("a multiplicação é", numero3)
+numero4 = 2 / 20
+print("a divisão é", numero4)
